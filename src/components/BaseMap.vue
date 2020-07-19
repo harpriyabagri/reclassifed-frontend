75e6405fd208757b06650f2091cf5a83c587a0e3
harpriya <harpriya@harpriyas-MacBook-Pro.local>
2020-07-19 12:03:45 -0700

    mapbox gl expressions tutorial

--- src/components/BaseMap.vue
+++ src/components/BaseMap.vue
@@ -11,11 +11,16 @@ export default {
     return {
       accessToken:
         "pk.eyJ1IjoiaGFycHJpeWFiYWdyaSIsImEiOiJja2NxeHR6dWgwcjJnMnJtMXhreWN4MWoxIn0.wW12qOAMq730lfuLyXb9nw",
+      tempData: [
+        ["headline example", [12, 12], "www.facebook.com"],
+        ["banaas", [22, 22], "www.reddit.com"],
+        ["duders", [22, 100], "www.instagram.com"],
+      ],
     };
   },
   mounted() {
     mapboxgl.accessToken = this.accessToken;
-    var map = new mapboxgl.Map({  
+    var map = new mapboxgl.Map({
       container: "mapContainer",
       style: "mapbox://styles/harpriyabagri/ckcs1ofh31ejn1iqxzmqwppj2",
       zoom: 1,
@@ -23,6 +28,31 @@ export default {
 
     var nav = new mapboxgl.NavigationControl();
     map.addControl(nav, "top-right");
+
+    map.on("load", function() {
+      map.addLayer({
+        id: "historical-places",
+        type: "circle",
+        source: {
+          type: "vector",
+          url: "mapbox://harpriyabagri.ab3jqtro",
+        },
+        "source-layer": "HPC_landmarks-9r0qec",
+        paint: {
+          "circle-radius": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            10,
+            ["/", ["-", 2017, ["number", ["get", "Constructi"], 2017]], 30],
+            13,
+            ["/", ["-", 2017, ["number", ["get", "Constructi"], 2017]], 10],
+          ],
+          "circle-opacity": 0.8,
+          "circle-color": "rgb(171, 72, 33)",
+        },
+      });
+    });
   },
 };
 </script>
