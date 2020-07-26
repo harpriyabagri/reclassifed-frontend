0f34da541bd2cd01ca8cb57e858b678ffb658dfb
Janna Agustin <jannalouisea@gmail.com>
2020-07-26 17:34:40 -0400

    edits to dashboard button

--- src/components/UIOverlay.vue
+++ src/components/UIOverlay.vue
@@ -107,5 +107,37 @@ s
   height: 15px;
   margin-right: 10px;
 }
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  background-color: #242424;
+  color: rgb(228, 228, 228);
+  border-radius: 8px;
+}
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
 
 </style>
