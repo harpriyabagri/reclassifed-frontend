5733dff74b063bfedc6155d53536698970bd4fbf
Shayna <sgrose@sfu.ca>
2020-07-25 23:06:07 -0700

    modified modal opening/closing

--- src/components/UIOverlay.vue
+++ src/components/UIOverlay.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="UIOverlay">
     <div class="top-right">
-      <div class="button" id="aboutButton" @click="showAbout">About</div>
+      <div class="button" id="aboutButton" @click="showAbout = true">About</div>
       <div class="button">Dashboard</div>
     </div>
     <div class="bottom-left">
@@ -18,35 +18,37 @@
       </div>
     </div>
     <div class="top-centre">
-      <div id="aboutModal" class="modal">
+      <modal v-if="showAbout" id="aboutModal" class="modal">
         <div class="modal-content">
-          <span class="close" @click="close">&times;</span>
+          <span class="close" @click="showAbout = false">&times;</span>
           <h2>about newsworthy.ml</h2>
-          <p>Hey, thanks for checking out Newsworthy! This project is the brainchild of six fellows working at 
-            the Montreal-based AI4Good Lab. Following an intensive five-week curriculum of AI and machine learning, 
-            we had three weeks to implement Newsworthy as a working product. We hope you enjoy exploring Newsworthy 
+          <p>
+            Hey, thanks for checking out Newsworthy! This project is the brainchild of six fellows working at
+            the Montreal-based AI4Good Lab. Following an intensive five-week curriculum of AI and machine learning,
+            we had three weeks to implement Newsworthy as a working product. We hope you enjoy exploring Newsworthy
             as much as we enjoyed creating it!
-            <br><br>
-            Now more than ever before, information literacy is crucial. Newsworthy provides analytics on users 
-            reading habits and provides suggestions on how users can diversify their news intake to reduce bias, and 
-            improve their information literacy. Did you know that nearly half of original news stories are first broken by 
-            local news outlets? Despite this, local newspapers are being eradicated by larger national outlets.  Newsworthy 
+            <br />
+            <br />Now more than ever before, information literacy is crucial. Newsworthy provides analytics on users
+            reading habits and provides suggestions on how users can diversify their news intake to reduce bias, and
+            improve their information literacy. Did you know that nearly half of original news stories are first broken by
+            local news outlets? Despite this, local newspapers are being eradicated by larger national outlets. Newsworthy
             strives to provide an equal playing field for local newspapers, so they can get the attention they deserve.
-            We had three weeks to create Newsworthy, and think that if we had four, five, gosh forbid six weeks, we could 
-            do a lot more. 
-            <br><br>
-            Future implementations and ideas include _____ and ______.
-            <br><br>
-            Nobody is perfect, and we are no exception! We have identified several limitations of our project. 
-            (we can list these as a group and I can add it in.)</p>
+            We had three weeks to create Newsworthy, and think that if we had four, five, gosh forbid six weeks, we could
+            do a lot more.
+            <br />
+            <br />Future implementations and ideas include _____ and ______.
+            <br />
+            <br />Nobody is perfect, and we are no exception! We have identified several limitations of our project.
+            (we can list these as a group and I can add it in.)
+          </p>
         </div>
-      </div>
+      </modal>
       <div class="search">
-          <form action="">
-            <input type="text" placeholder="Search.." name="search">
-            <!-- <button type="submit"></button> -->
-          </form>
-        </div>
+        <form action>
+          <input type="text" placeholder="Search.." name="search" />
+          <!-- <button type="submit"></button> -->
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -68,12 +70,13 @@ export default {
         { category: "Crisis Updates", color: "#000000", index: 8 },
       ],
       active_filters: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      showAbout: false,
     };
   },
   methods: {
     filter(cat) {
       var i = cat.index;
-      this.$set(this.active_filters, i, !this.active_filters[i])
+      this.$set(this.active_filters, i, !this.active_filters[i]);
       this.$emit("revaluate-filters", this.active_filters);
     },
   },
@@ -155,22 +158,21 @@ export default {
   margin-right: 10px;
 }
 .modal {
-  display: none; 
-  position: fixed; 
-  z-index: 1; 
+  position: fixed;
+  z-index: 1;
   left: 0;
   top: 0;
   width: 100%;
-  height: 100%; 
-  overflow: auto; 
-  background-color: rgba(0,0,0,0.4);
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
 }
 .modal-content {
   background-color: #fefefe;
-  margin: 15% auto; 
+  margin: 15% auto;
   padding: 20px;
   border: 1px solid #888;
-  width: 80%; 
+  width: 80%;
   background-color: #242424;
   color: rgb(228, 228, 228);
   border-radius: 8px;
@@ -187,7 +189,7 @@ export default {
   text-decoration: none;
   cursor: pointer;
 }
-input[type='text'] { 
+input[type="text"] {
   background: rgba(0, 0, 0, 0);
   background-color: none;
   color: #414141;
@@ -195,7 +197,7 @@ input[type='text'] {
   width: 200px;
   letter-spacing: 0.4px;
   /* border-radius: 5px; */
-  font-size: 16px; 
+  font-size: 16px;
   letter-spacing: 0.4px;
   outline: none;
   box-sizing: border-box;
@@ -213,7 +215,7 @@ input[type='text'] {
   border: 1px solid grey;
   border-radius: 5px;
 } */
-::placeholder { 
+::placeholder {
   color: #b0b0b0;
   font-family: "Times New Roman";
 }
