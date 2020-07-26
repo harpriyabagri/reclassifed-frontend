c0d831e303d16c2dedca5f7da4c02edee4778fba
harpriya <harpriya@harpriyas-MacBook-Pro.local>
2020-07-25 21:06:54 -0700

    pulling about button

--- src/components/UIOverlay.vue
+++ src/components/UIOverlay.vue
@@ -2,7 +2,6 @@
   <div class="UIOverlay">
     <div class="top-right">
       <div class="button">Dashboard</div>
-      <div class="button" id="aboutButton" @click="showAbout">About</div>
     </div>
     <div class="bottom-left">
       <div
@@ -15,41 +14,6 @@
         <div class="circle" :style="{ backgroundColor: cat.color }"></div>
         <div class="category-title">{{ cat.category }}</div>
       </div>
-    </div>
-    <div class="top-centre">
-      <div id="aboutModal" class="modal">
-        <div class="modal-content">
-          <span class="close" @click="close">&times;</span>
-          <!-- will be replaced with actual text -->
-          <h2>about newsworthy.ml</h2>
-          <p>Hey, thanks for checking out Newsworthy! This project is the brainchild of six fellows working at 
-            the Montreal-based AI4Good Lab. Following an intensive five-week curriculum of AI and machine learning, 
-            we had three weeks to implement Newsworthy as a working product. We hope you enjoy exploring Newsworthy 
-            as much as we enjoyed creating it!
-            <br><br>
-            Now more than ever before, information literacy is crucial. Newsworthy provides analytics on users 
-            reading habits and provides suggestions on how users can diversify their news intake to reduce bias, and 
-            improve their information literacy. Did you know that nearly half of original news stories are first broken by 
-            local news outlets? Despite this, local newspapers are being eradicated by larger national outlets.  Newsworthy 
-            strives to provide an equal playing field for local newspapers, so they can get the attention they deserve.
-            We had three weeks to create Newsworthy, and think that if we had four, five, gosh forbid six weeks, we could 
-            do a lot more. 
-            <br><br>
-            Future implementations and ideas include _____ and ______.
-            <br><br>
-            Nobody is perfect, and we are no exception! We have identified several limitations of our project. 
-            (we can list these as a group and I can add it in.)</p>
-        </div>
-      </div>
-      
-    </div>
-    <div class="top-left">
-      <div class="search">
-          <form action="">
-            <input type="text" placeholder="Search.." name="search">
-            <!-- <button type="submit"></button> -->
-          </form>
-        </div>
     </div>
   </div>
 </template>
@@ -79,18 +43,9 @@ export default {
       this.$set(this.active_filters, i, !this.active_filters[i])
       this.$emit("revaluate-filters", this.active_filters);
     },
-    showAbout() {
-      var modal = document.getElementById("aboutModal")
-      modal.style.display = "block"
-    },
-    close() {
-      var modal = document.getElementById("aboutModal")
-      modal.style.display = "none"
-    }
   },
   mounted() {},
 };
-
 </script>
 
 
@@ -116,18 +71,6 @@ export default {
   bottom: 50px;
   left: 10px;
 }
-.top-centre {
-  pointer-events: auto;
-  position: absolute;
-  left: 40%;
-  top: 10px;
-}
-.top-left {
-  pointer-events: auto;
-  position: absolute;
-  left: 10px;
-  top: 10px;
-}
 .button {
   letter-spacing: 0.4px;
   background-color: none;
@@ -146,6 +89,7 @@ export default {
 .button:hover {
   background-color: #f2f2f2;
 }
+
 .category {
   letter-spacing: 0.4px;
   background-color: none;
@@ -170,66 +114,5 @@ export default {
   width: 15px;
   height: 15px;
   margin-right: 10px;
-}
-.modal {
-  display: none; 
-  position: fixed; 
-  z-index: 1; 
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%; 
-  overflow: auto; 
-  background-color: rgb(0,0,0); 
-  background-color: rgba(0,0,0,0.4); 
-}
-.modal-content {
-  background-color: #fefefe;
-  margin: 15% auto; 
-  padding: 20px;
-  border: 1px solid #888;
-  width: 80%; 
-}
-.close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
-}
-.close:hover,
-.close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
-}
-input[type='text'] { 
-  background: rgba(0, 0, 0, 0);
-  background-color: none;
-  color: #414141;
-  height: 30px;
-  width: 160px;
-  letter-spacing: 0.4px;
-  border-radius: 5px;
-  font-size: 16px; 
-  letter-spacing: 0.4px;
-  border-radius: 5px;
-  box-shadow: 0 3px 6px #3535353b;
-  outline: none;
-  border-width: 0px;
-  box-sizing: border-box;
-  font-family: "Times New Roman";
-  justify-content: flex-start;
-  padding: 0 5px;
-}
-input[type='text']:hover {
-  background-color: #f2f2f2;
-}
-input[type='text']:focus {
-  border: 1px solid grey;
-  border-radius: 5px;
-}
-::placeholder { 
-  color: #414141;
-  font-family: "Times New Roman";
 }
 </style>
